591d00f0508e8bac00495e25954fea9d3dd9b624
Instamaikle <misasapka1@gmail.com>
2024-08-21 23:05:29 +0300

    Програма Обробка некоректного формату числа готова

--- FormatNumbers/Program.cs
+++ FormatNumbers/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+Console.WriteLine("Дз: Обробка некоректного формату числа");
+Console.WriteLine("Введіть в консоль тільки число:");
+
+try
+{
+int Numbers = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine($"Ви ввели число - {Numbers}");
+}
+catch(FormatException)
+{
+    Console.WriteLine("Введений вами текст не є числом");
+}
